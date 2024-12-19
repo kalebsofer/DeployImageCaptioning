ec2a9f2f52ae80eea73adb0f14cdeea046a5571c
Kaleb Sofer <kalebsofer@gmail.com>
2024-12-19 15:57:00 +0000

    docker-compose traefik update

--- nginx/setup-ssl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Obtain SSL certificates if they do not exist
-if [ ! -f "/etc/letsencrypt/live/inventiveimagecaption.com/fullchain.pem" ]; then
-    certbot certonly --standalone -d inventiveimagecaption.com -d www.inventiveimagecaption.com --non-interactive --agree-tos --email kaleb@softmax.io
-fi
-
-# Start Nginx with the SSL certificates
-nginx -g 'daemon off;'
